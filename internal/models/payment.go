098471e95ca99f5efdf5e5391c7fb346f2ccfbc1
Katerina Zakharova <kzakharova96@yandex.com>
2024-09-17 13:05:11 +0300

    added initial script for db migration

--- internal/models/payment.go
+++ internal/models/payment.go
@@ -2,13 +2,13 @@ package models
 
 type Payment struct {
 	Transaction  string `json:"transaction"`
-	ReqestID     string `json:"request_id"`
+	RequestID     string `json:"request_id"`
 	Currency     string `json:"currency"`
 	Provider     string `json:"provider"`
 	Amount       int    `json:"amount"`
 	PaymentDT    int    `json:"payment_dt"`
 	Bank         string `json:"bank"`
 	DeliveryCost int    `json:"delivery_cost"`
-	GoodsTatal   int    `json:"goods_total"`
-	Custom_fee   int    `json:"custom_fee"`
+	GoodsTotal   int    `json:"goods_total"`
+	CustomFee   int    `json:"custom_fee"`
 }
